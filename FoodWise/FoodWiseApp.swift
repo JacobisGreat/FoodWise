142a2c1ddf090aad539cb103efe87ed18fcae167
Aditya Makhija <354139685@educ.dpcdsb.org>
2025-08-02 12:41:25 -0400

    Initial Commit

---
+++ FoodWise/FoodWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWiseApp.swift
+//  FoodWise
+//
+//  Created by Aditya Makhija on 2025-08-02.
+//
+
+import SwiftUI
+
+@main
+struct FoodWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
